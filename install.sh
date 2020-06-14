c4093f8f29b6ac41bca37249d0e401f2b48dac8f
Oskar Rough <oskar@rough.dk>
2020-06-14 09:15:42 +0200

    Remove unused fish functions

--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@
 
 # Delete existing configuration files
 rm ~/.config/fish/config.fish
-rm ~/.config/fish/functions/compress.fish
-rm ~/.config/fish/functions/extract.fish
+rm ~/.vimrc
 rm ~/.config/nvim/init.vim
 rm ~/.config/i3/config
-rm ~/.vimrc
 
 # Create folders (if they don't exist)
 mkdir -p ~/.config/fish
@@ -17,8 +15,6 @@ mkdir -p ~/.config/i3
 dotfiles=${PWD}
 
 ln -s $dotfiles/fish/config.fish ~/.config/fish/config.fish
-ln -s $dotfiles/fish/functions/compress.fish ~/.config/fish/functions/compress.fish
-ln -s $dotfiles/fish/functions/extract.fish ~/.config/fish/functions/extract.fish
 ln -s $dotfiles/.vimrc ~/.vimrc
 ln -s $dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -s $dotfiles/i3/config ~/.config/i3/config
