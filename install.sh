39cdfc282b2574d91de13adeec62f006d4d2c797
Oskar Rough <oskar@rough.dk>
2022-01-14 16:30:15 +0100

    Disable i3 for now

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@
 rm ~/.config/fish/config.fish
 rm ~/.vimrc
 rm ~/.config/nvim/init.vim
-rm ~/.config/i3/config
+# rm ~/.config/i3/config
 
 # Create folders (if they don't exist)
 mkdir -p ~/.config/fish
 mkdir -p ~/.config/nvim
-mkdir -p ~/.config/i3
+# mkdir -p ~/.config/i3
 
 # Create symlinks
 dotfiles=${PWD}
@@ -17,6 +17,6 @@ dotfiles=${PWD}
 ln -s $dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -s $dotfiles/.vimrc ~/.vimrc
 ln -s $dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -s $dotfiles/i3/config ~/.config/i3/config
+# ln -s $dotfiles/i3/config ~/.config/i3/config
 
 echo 'Symlinks have been created.'
