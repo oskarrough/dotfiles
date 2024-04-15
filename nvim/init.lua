2ba9be88dc3df25d25fc527c02a31e15a87a6764
Oskar Rough <oskar@rough.dk>
2024-04-15 15:28:25 +0200

    Checkpoint

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,52 +1,87 @@
-local paq = require("paq")
+-- copy/paste setup from lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-require "paq" {
-  "savq/paq-nvim", -- Let Paq manage itself
+-- set mapleader before lazy
+vim.g.mapleader = ' ' -- <space> as leader
 
-  -- LSP Support
-  { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
-  -- automatic lsp installs
-  { 'williamboman/mason.nvim' },
-  { 'williamboman/mason-lspconfig.nvim' },
-  -- lsp deps
-  { 'neovim/nvim-lspconfig' },
-  { 'hrsh7th/nvim-cmp' },
-  { 'hrsh7th/cmp-nvim-lsp' },
-  { 'L3MON4D3/LuaSnip' },
-  -- { 'hrsh7th/cmp-buffer' },
-  -- { 'hrsh7th/cmp-path' },
-  -- { 'hrsh7th/cmp-nvim-lua' },
-  -- { 'rafamadriz/friendly-snippets' },
+require("lazy").setup({
+	{
+		'nanotech/jellybeans.vim',
+		lazy = false,
+		config = function()
+			vim.cmd([[colorscheme jellybeans]])
+		end
+	},
+	--'morhetz/gruvbox',
 
-  -- Helpful ones
-  "tpope/vim-surround",
-  "tpope/vim-commentary",
-  'sgur/vim-editorconfig',
-  'junegunn/fzf',
-  'junegunn/fzf.vim',
+	{ 'williamboman/mason.nvim' },
+	{ 'williamboman/mason-lspconfig.nvim' },
 
-  -- Do we need these?
-  'sheerun/vim-polyglot',
-  'github/copilot.vim',
-  "folke/which-key.nvim",
-  'nvim-lualine/lualine.nvim',
-  'prettier/vim-prettier',
-  'morhetz/gruvbox',
-  'nanotech/jellybeans.vim',
-    'evanleck/vim-svelte';
-  -- 'tpope/vim-fugitive' " git stuff
-  -- 'w0rp/ale' " linter
-  -- 'suy/vim-context-commentstring' " proper comments for <script> tags etc.
-  -- 'brooth/far.vim' " search/replace
-  -- 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
-  -- 'neoclide/coc.vim';
-  -- 'projekt0n/github-nvim-theme';
-    -- 'wuelnerdotexe/vim-astro';
+	-- LSP Support
+	{ 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+	{ 'neovim/nvim-lspconfig' },
+	-- autocompletion?! why is not built-in....
+	{
+		"hrsh7th/nvim-cmp",
+		-- load cmp on InsertEnter
+		event = "InsertEnter",
+		-- these dependencies will only be loaded when cmp loads
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+		},
+		config = function()
+		end,
+	},
+	{ "L3MON4D3/LuaSnip", version = "v2.*" },
+
+	'rafamadriz/friendly-snippets', -- community snippets for all the languages
+
+	-- -- Helpful ones
+	"tpope/vim-surround",
+	"tpope/vim-commentary",
+	"tpope/vim-sleuth",
+	'junegunn/fzf',
+	'junegunn/fzf.vim',
+
+	-- -- Do we need these?
+	--'sheerun/vim-polyglot',
+	'folke/which-key.nvim',
+	-- 'github/copilot.vim',
+	--'sgur/vim-editorconfig', -- dont think this is needed anylonger, default is nvim
+	{
+		'nvim-lualine/lualine.nvim',
+		--dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
+	-- 'prettier/vim-prettier',
+	--  'evanleck/vim-svelte';
+	-- 'tpope/vim-fugitive' " git stuff
+	-- 'suy/vim-context-commentstring' " proper comments for <script> tags etc.
+	-- 'projekt0n/github-nvim-theme';
+	-- 'wuelnerdotexe/vim-astro';
+	-- 'w0rp/ale' " linter
+	-- 'brooth/far.vim' " search/replace
+	-- 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
+	-- 'neoclide/coc.vim';
+})
+
+require('lualine').setup {
+	options = {
+		theme = 'jellybeans'
+	}
 }
-
-require('lualine').setup {}
 require('which-key').setup {}
-
 
 -- LSP setup
 local lsp_zero = require("lsp-zero")
@@ -55,11 +90,11 @@ lsp_zero.preset("recommended")
 -- LSP mason integration
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  -- Replace the language servers listed here with the ones you want to install
-  -- ensure_installed = {'tsserver', 'rust_analyzer'},
-  handlers = {
-    lsp_zero.default_setup,
-  },
+	-- Replace the language servers listed here with the ones you want to install
+	-- ensure_installed = {'tsserver', 'rust_analyzer'},
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
 require('mason-lspconfig').setup({})
 
@@ -68,23 +103,23 @@ local lua_opts = lsp_zero.nvim_lua_ls()
 require('lspconfig').lua_ls.setup(lua_opts)
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  lsp_zero.default_keymaps({ buffer = bufnr })
-  -- local opts = {buffer = bufnr, remap = false}
-  -- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  -- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  -- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  -- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  -- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  -- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  -- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  -- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	-- see :help lsp-zero-keybindings
+	lsp_zero.default_keymaps({ buffer = bufnr })
+	-- local opts = {buffer = bufnr, remap = false}
+	-- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	-- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	-- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	-- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+	-- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	-- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+	-- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+	-- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+	-- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+	-- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
+local luasnip = require("luasnip")
+local cmp = require("cmp")
 
-
--- local cmp = require('cmp')
 -- local cmp_select = { behavior = cmp.SelectBehavior.Select }
 -- local cmp_mappings = lsp_zero.defaults.cmp_mappings({
 --   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
@@ -92,6 +127,53 @@ end)
 --   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
 --   ["<C-Space>"] = cmp.mapping.complete(),
 -- })
+--
+
+-- testing this out, not sure
+cmp.setup({
+  -- ... Your other configuration ...
+
+  mapping = {
+    -- ... Your other mappings ...
+   ['<CR>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            if luasnip.expandable() then
+                luasnip.expand()
+            else
+                cmp.confirm({
+                    select = true,
+                })
+            end
+        else
+            fallback()
+        end
+    end),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    -- ... Your other mappings ...
+  },
+
+  -- ... Your other configuration ...
+})
 
 -- disable completion with tab
 -- this helps with copilot setup
@@ -110,12 +192,11 @@ end)
 --     info = 'I'
 --   }
 -- })
-
 lsp_zero.setup()
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.swapfile = false 
+vim.opt.swapfile = false
 
 -- Indention
 vim.opt.smartindent = true
@@ -148,18 +229,13 @@ vim.opt.scrolloff = 2 -- Start scrolling X lines before top/end
 
 -- Colors
 vim.opt.termguicolors = true
-vim.cmd('colorscheme jellybeans')
-vim.cmd('colorscheme jellybeans')
 
--- Keybinds
-vim.g.mapleader = ' ' -- <space> as leader
-
--- Navigation 
+-- Navigation
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<space>w', '<cmd>write<cr>', { desc = 'Save' })
 vim.keymap.set('n', '<leader>p', '<cmd>Files<cr>') -- or GitFiles
 vim.keymap.set('n', '<leader>b', '<cmd>Buffers<cr>')
-vim.keymap.set('n', '<leader>a', '<cmd>Ag<cr>') -- from fzf.vim
+vim.keymap.set('n', '<leader>a', '<cmd>Ag<cr>')    -- from fzf.vim
 vim.keymap.set('n', '<leader>w', '<cmd>:w<cr>')
 vim.keymap.set('i', '<c-x><c-l>', '<plug>(fzf-complete-line)')
 -- nmap <leader>h :History<CR>
@@ -170,7 +246,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 --vim.keymap.set('n', '<leader>f', '<cmd>:Prettier<cr>')
 vim.keymap.set('n', '<leader>f', function()
-  vim.lsp.buf.format()
+	vim.lsp.buf.format()
 end)
 
 -- When moving up and down center it as well
@@ -199,8 +275,10 @@ vim.keymap.set('n', '<leader>ev', '<cmd>:edit $MYVIMRC<cr>')
 -- Setup svelte
 vim.cmd [[
 let g:svelte_preprocessor_tags = [
-  \ { 'name': 'ts', 'tag': 'script', 'as': 'typescript' }
-  \ ]
+\ { 'name': 'ts', 'tag': 'script', 'as': 'typescript' }
+\ ]
 let g:svelte_preprocessors = ['ts']
 ]]
+
+
 
